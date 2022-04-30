2785ec270c415741388dd536ad4f5316b312ee73
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-04-30 12:41:06 +0430

    Add not found middleware

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const tasks = require('./tasks');
+const notFound = require('@middlewares/not-found');
 
 module.exports = (app) => {
     app.use('/api/v1/tasks', tasks);
+    
+    // middleware
+    app.use(notFound);
 }
